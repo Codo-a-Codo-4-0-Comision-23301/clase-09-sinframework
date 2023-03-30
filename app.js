a2d4d95db149bac628aad93b6c534ebd65b0d7c5
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2023-03-30 19:54:58 -0300

    wip

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-const e = React.createElement;
-function Button() {
-    return e(
-      'button',
-      {  },
-      'Boton'
-    );
-}
+//const e = React.createElement;
+import Button from "./button.js";
 
 let myApp = document.getElementById("myApp");
 myApp.innerHTML = "Hello World";
@@ -13,4 +7,4 @@ myApp.innerHTML = myApp.innerHTML + '<div id="reactApp"></div>';
 
 let myReactApp = document.getElementById("reactApp");
 const root = ReactDOM.createRoot(myReactApp);
-//root.render(e(Button));
+//root.render(React.createElement(Button));
