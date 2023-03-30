8673892cea512282bc31280ce8263b2b96fb7a77
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2023-03-30 19:53:20 -0300

    wip

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-import { createRoot } from 'react-dom/client';
+const e = React.createElement;
+function Button() {
+    return e(
+      'button',
+      {  },
+      'Boton'
+    );
+}
 
 let myApp = document.getElementById("myApp");
-
 myApp.innerHTML = "Hello World";
-
 myApp.innerHTML = myApp.innerHTML + '<div id="reactApp"></div>';
 
-const root = createRoot(document.getElementById("reactApp"));
-root.render("<h1>Hellow world From React</h1>");
+let myReactApp = document.getElementById("reactApp");
+const root = ReactDOM.createRoot(myReactApp);
+//root.render(e(Button));
